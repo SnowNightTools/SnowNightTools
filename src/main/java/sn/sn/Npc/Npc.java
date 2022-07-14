78374dc898270c868c375a95c2da22cc33c51577
flutterma <965030558@qq.com>
2022-07-14 18:03:38 +0800

    flutterma加入项目编辑

--- src/main/java/sn/sn/Npc/Npc.java
+++ src/main/java/sn/sn/Npc/Npc.java
@@ -5,7 +5,7 @@ import org.bukkit.entity.EntityType;
 import javax.swing.text.Position;
 
 public class Npc {
-
+//2022年7月14日18:02:13 flutterma加入项目编辑
     Memory memory;
 
     String firstname, lastname;
